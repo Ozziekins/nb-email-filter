c16b75eb20e6d52eb57342e6ca5afb3929004c19
Ozziekins <o.okonicha@innopolis.university>
2023-12-20 12:13:26 +0400

    changed api back to localhost

--- src/components/Section.js
+++ src/components/Section.js
@@ -53,7 +53,7 @@ export const Section = () => {
 
     const handlePredictClick = () => {
       if (fileContent.trim() !== '') {
-        fetch('/.netlify/functions/api', {
+        fetch('http://127.0.0.1:5000/predict', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
