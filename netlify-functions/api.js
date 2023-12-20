f642b6daa044ed87a3b8f58813aa8d17b1ee34cc
Ozziekins <o.okonicha@innopolis.university>
2023-12-20 11:36:40 +0400

    adding netlify config

---
+++ netlify-functions/api.js
@@ -0,0 +1,17 @@
+// netlify-functions/api.js
+const { execSync } = require('child_process');
+
+exports.handler = async function (event, context) {
+  try {
+    const result = execSync('python3 nb_model/api.py').toString('utf-8');
+    return {
+      statusCode: 200,
+      body: result,
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: error.toString(),
+    };
+  }
+};
