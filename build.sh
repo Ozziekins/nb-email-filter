06b6197459f6664f004f433c5420708b840321c2
Ozziekins <o.okonicha@innopolis.university>
2023-12-20 22:57:00 +0400

    api path debugging

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y python3.8
+apt-get install -y python3-pip
--- netlify-functions/api/api.js
+++ netlify-functions/api/api.js
@@ -4,13 +4,7 @@ const path = require('path');
 
 exports.handler = async function (event, context) {
   try {
-    // Set the path to the directory containing api.py
-    const nbModelPath = path.join(__dirname, '..', 'nb_model');
-
-    // Run the command with cd to the nb_model directory
-    const dir = execSync(`ls`).toString('utf-8');
-    console.log(dir)
-    const result = execSync(`cd nb_model/ && python api.py`).toString('utf-8');
+    const result = execSync(`python ../../nb_model/api.py`).toString('utf-8');
 
     return {
       statusCode: 200,
