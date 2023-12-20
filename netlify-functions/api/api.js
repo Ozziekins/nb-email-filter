74dc0c31ef3216c609e15cd77884643731341dee
Ozziekins <o.okonicha@innopolis.university>
2023-12-20 22:24:53 +0400

    api path

--- netlify-functions/api/api.js
+++ netlify-functions/api/api.js
@@ -2,19 +2,17 @@
 const { execSync } = require('child_process');
 
 exports.handler = async function (event, context) {
-  console.log('Function invoked!');
   try {
-    const result = execSync('python3 nb_model/api.py').toString('utf-8');
+    const result = execSync('python3 ../../../nb_model/api.py').toString('utf-8');
+
     return {
       statusCode: 200,
       body: result,
     };
   } catch (error) {
-    console.error('Function error:', error);
     return {
       statusCode: 500,
       body: error.toString(),
     };
   }
 };
-
