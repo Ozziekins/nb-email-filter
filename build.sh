5ea37dfcd26b4731dc0864aa6f85b2f9ec15c130
Ozziekins <o.okonicha@innopolis.university>
2023-12-20 22:36:29 +0400

    changing python version

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-apt-get update
-apt-get install -y python3.8
-apt-get install -y python3-pip
--- netlify-functions/api/api.js
+++ netlify-functions/api/api.js
@@ -8,7 +8,7 @@ exports.handler = async function (event, context) {
     const nbModelPath = path.join(__dirname, '..', 'nb_model');
 
     // Run the command with cd to the nb_model directory
-    const result = execSync(`cd ${nbModelPath} && python3 api.py`).toString('utf-8');
+    const result = execSync(`cd ${nbModelPath} && python api.py`).toString('utf-8');
 
     return {
       statusCode: 200,
